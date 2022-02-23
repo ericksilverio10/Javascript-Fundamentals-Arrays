77ed4c353deefed5bd8f9919465199a8f8e1f89a
Erick <erick1junior@hotmail.com>
2022-02-23 01:50:06 -0300

    Add files

---
+++ lista-duas-dimensoes.js
@@ -0,0 +1,15 @@
+// Array de duas dimensões
+const alunos = ['João', 'Juliana', 'Caio', 'Ana'];
+
+const medias = [10, 7, 9, 6];
+
+let listaFinal = [alunos, medias];
+
+//listaFinal[0] = alunos
+//listaFinal[0][0] = alunos[0] = 'João'
+
+//listaFinal[1] = medias
+//listaFinal[1][0] = medias[0] = '10'
+
+          //Lista alunos[0]                    //Lista medias[0]
+console.log(`${listaFinal[0][0]}, sua média é ${listaFinal[1][0]}!`);
