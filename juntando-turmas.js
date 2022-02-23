51ba24c1b776d15dc44ea6afba36aa88a1127988
Erick <erick1junior@hotmail.com>
2022-02-23 01:37:30 -0300

    Add files

---
+++ juntando-turmas.js
@@ -0,0 +1,6 @@
+const salaDePython = ['Melissa', 'Ana', 'Camila', 'Erick'];
+const salaDeJS = ['Gustavo', 'Larissa', 'Pedro', 'Julia'];
+
+const salasJuntas = salaDePython.concat(salaDeJS);
+
+console.log(`Essa é as salas juntas: ${salasJuntas}`);
